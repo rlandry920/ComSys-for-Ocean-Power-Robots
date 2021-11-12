b0ba4c67cc207807641259b2768f7b992e243c72
Michael Fuhrer <mfuhrer@vt.edu>
2021-11-12 02:35:56 -0500

    Updated for demo presentation

--- robot_flask.py
+++ robot_flask.py
@@ -7,6 +7,7 @@ import requests
 
 app = Flask(__name__)
 
+FEATHER_M0 = None
 
 @app.route('/', methods=['GET', 'POST', 'DELETE'])
 def output():
@@ -35,37 +36,71 @@ def goToCoordinates():
     elif(longitude == ""):
         return "Invalid longitude"
     else:
-        r = request("http://<FeatherM0>:<port>/move?type=coords&latitude=" +
-                    str(latitude)+"&longitude="+str(longitude))
+        move_params = {
+            "type": "coords",
+            "latitude": latitude,
+            "longitude": longitude
+            }
+        r = requests.post(f"http://{FEATHER_M0}/move", data=move_params)
+        print(r.text)
         return r.text
 
 
 @app.route('/turnLeft', methods=['POST', 'DELETE'])
 def turnLeft():
-    r = requests.post(
-        "http://<FeatherM0>:<port>/move?type=direct&direction=left")
+    move_params = {
+        "type": "direct",
+        "direction": "left"
+    }
+    r = requests.post(f"http://{FEATHER_M0}/move", data=move_params)
+    print(r.text)
     return r.text
 
 
 @app.route('/turnRight', methods=['POST', 'DELETE'])
 def turnRight():
-    r = requests.post(
-        "http://<FeatherM0>:<port>/move?type=direct&direction=right")
+    move_params = {
+        "type": "direct",
+        "direction": "right"
+    }
+    r = requests.post(f"http://{FEATHER_M0}/move", data=move_params)
+    print(r.text)
     return r.text
 
 
 @app.route('/moveForward', methods=['POST', 'DELETE'])
 def moveForward():
-    r = requests.post(
-        "http://<FeatherM0>:<port>/move?type=direct&direction=forward")
+    move_params = {
+        "type": "direct",
+        "direction": "forward"
+    }
+    r = requests.post(f"http://{FEATHER_M0}/move", data=move_params)
+    print(r.text)
     return r.text
 
 
 @app.route('/moveBackward', methods=['POST', 'DELETE'])
 def moveBackward():
-    r = requests.post(
-        "http://<FeatherM0>:<port>/move?type=direct&direction=backward")
+    move_params = {
+        "type": "direct",
+        "direction": "backward"
+    }
+    r = requests.post(f"http://{FEATHER_M0}/move", data=move_params)
+    print(r.text)
     return r.text
+
+
+@app.route('/heartbeat', methods=['POST'])
+def get_robot_heartbeat():
+    status = request.form['status']
+    latitude = float(request.form['latitude'])
+    longitude = float(request.form['longitude'])
+
+    print("Heartbeat Received")
+    print(f"Robot Status: {status}")
+    print(f"Latitude: {latitude} | Longitude: {longitude}")
+
+    return "Heartbeat ACKd"
 
 
 @app.route('/stopTurnLeft', methods=['POST', 'DELETE'])
@@ -88,12 +123,18 @@ def stopMoveBackward():
     return "Robot stopped moving backward"
 
 
-@app.route('/randomNum', methods=['POST', 'DELETE'])
-def randomNum():
-    r = requests.post("http://<FeatherM0>:<port>/query")
-    return r.text
+# Cannot implement below for demo due to limitations on how the Arduino acts as an I2C-slave :(
+# @app.route('/randomNum', methods=['POST', 'DELETE'])
+# def randomNum():
+#     r = requests.post("http://<FeatherM0>:<port>/query")
+#     return r.text
+
+
+def get_feather_ip():
+    global FEATHER_M0
+    FEATHER_M0 = input("Please enter the IP and port of the FeatherM0 Board's HTTP Server: ")
 
 
 if __name__ == '__main__':
-    # run!
-    app.run()
+    get_feather_ip()
+    app.run(host="0.0.0.0")  # Set host to 0.0.0.0 to run flask externally
